c4087627a6197ddb3786698ee8e3d8a6b1fc53cd
Danilo Wierling <danilo.wierling@gmail.com>
2017-04-02 23:27:03 +0200

    2nd Commit

---
+++ Notify/Notify/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Notify
+//
+//  Created by Danilo Wierling on 02.04.17.
+//  Copyright © 2017 Danilo Wierling. All rights reserved.
+//
+
+import Foundation
+
+class Task{
+    var name = ""
+    var important = false
+}
